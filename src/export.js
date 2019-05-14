e4a7f25568f77d13373aa15b5585f53f8b36f541
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-14 07:52:27 +0000

    Deploy adrianjost/feathersjs-filter-ui to github.com/adrianjost/feathersjs-filter-ui.git:gh-pages

--- src/export.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import './base.js'
-import VueCustomElement from "vue-custom-element";
-import Module from './components/filter';
-
-Vue.use(VueCustomElement);
-Vue.customElement('feathers-filter', Module);
